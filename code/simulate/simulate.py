ad26e201bbb1f20b0b303a1e4a183bcb83bff89d
Nathan Smith <nwraysmith@gmail.com>
2021-05-10 11:59:04 -0700

    rename files and update simulate.py

--- code/simulate/simulate.py
+++ code/simulate/simulate.py
@@ -7,13 +7,14 @@ import psutil
 import time
 
 
-def simulate_stream(df, cumulative=True, batch_size=10, model=None):
+def simulate_stream(df, colname="_value", cumulative=True, batch_size=10, model=None):
     """Simulated data stream form a pandas dataframe with an
     an algorithm that can be applied. The algorithm can be applied
     on the cumulative dataframe or only the new batch of data.
 
     Keyword arguments:
     df -- the entire dataset, pandas.DataFrame
+    colname -- column name with values to analyze, string (default = '_values')
     cumulative -- if algorithm should be applied in
         batch or cumulative, boolean (default True)
     batch_size -- batch size, int (default = 10)
@@ -35,17 +36,24 @@ def simulate_stream(df, cumulative=True, batch_size=10, model=None):
     }
 
     counter = 0
-    for i in range(batch_size, df.shape[0], batch_size):
+    for i in range(0, df.shape[0] - batch_size, batch_size):
 
         # Time taking
         start_t = time.perf_counter()
+
+        if cumulative:
+            start = 0
+        else:
+            start = i
+
+        end = i + batch_size
 
         # Model
         if model:
             # TODO: write code for algorithm
             pass
         else:
-            temp_results = X.loc[0:i, "_value"].mean()
+            temp_results = df.loc[start:end, "_value"].mean()
 
         # Memory and time taking
         t = time.perf_counter() - start_t
@@ -55,15 +63,17 @@ def simulate_stream(df, cumulative=True, batch_size=10, model=None):
         output["batch_num"].append(counter)
         output["time_measures"].append(t)
         output["memory_measures"].append(m)
-        output["model_results"].append()
+        output["model_results"].append(temp_results)
 
         counter += 1
 
     # Compute average statistics
-    output["time_avg"] = np.mean(output["time_measures"])  # average time in seconds
-    output["memory_avg"] = np.mean(
-        output["memory_measures"]
-    )  # average memory usage in Byte
-    output["model_avg"] = np.mean(output["model"])  # average model values
+    # time in seconds
+    output["time_avg"] = np.mean(output["time_measures"])
+    # memory usage in Byte
+    output["memory_avg"] = np.mean(output["memory_measures"])
+    # average model results
+    # TODO: update when model algorithm is functional - potentially need to iterate list
+    output["model_avg"] = np.mean(output["model_results"])
 
     return output
