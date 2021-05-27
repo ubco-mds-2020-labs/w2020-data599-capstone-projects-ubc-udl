92b86c2cc8fc5fee95e40605df64e60c09663d55
Mitch Harris <mitchharris@live.ca>
2021-05-27 09:51:17 -0700

    add predict .py

---
+++ code/model/sensor_predict.py
@@ -0,0 +1,41 @@
+"""
+asdf
+"""
+
+from tensorflow import keras
+
+import pandas as pd
+import numpy as np
+
+THRESHOLD = 1.5
+
+
+def make_prediction(model_id, x_data, time_stamps):
+    """
+    model_id: string, name of the model to load
+    x_data: np array of shape (num_rows, time_steps, 1)
+    time_stamps: time_stamps: list, timestamps of prediction points, find this out before passing
+    """
+    # make predictions
+    model = keras.models.load_model(f"../../../models/{model_id}")
+    x_test_pred = model.predict(x_data, verbose=0)
+
+    # format predictions
+    test_mae_loss = np.mean(np.abs(x_test_pred - x_data), axis=1)
+    test_score_df = pd.DataFrame(time_stamps, columns=["Timestamp"])
+    test_score_df["loss"] = test_mae_loss
+    test_score_df["threshold"] = THRESHOLD
+    test_score_df["anomaly"] = test_score_df["loss"] > test_score_df["threshold"]
+
+    return_data = {
+        "Timestamp": test_score_df["Timestamp"].values.tolist(),
+        "loss": test_score_df["loss"].values.tolist(),
+        "threshold": test_score_df["threshold"].values.tolist(),
+        "anomaly": test_score_df["anomaly"].values.tolist(),
+    }
+
+    return {
+        "version": "0.0.1",
+        "data": return_data,
+        "model_id": model_id,
+    }
