aa6943e03eaeeac253833b0230309d3692b51d4b
Mitch Harris <mitchharris@live.ca>
2021-06-15 18:02:12 -0700

    clean predict function

--- code/model/model_predict.py
+++ code/model/model_predict.py
@@ -8,7 +8,9 @@ import pandas as pd
 import numpy as np
 
 
-def make_prediction(model_id, x_data, time_stamps, threshold, model_path):
+def make_prediction(
+    model_id, x_data, time_stamps, threshold, model_path, anomaly_type="anomaly"
+):
     """
     model_id: string, name of the model to load
     x_data: np array of shape (num_rows, time_steps, 1)
@@ -16,6 +18,7 @@ def make_prediction(model_id, x_data, time_stamps, threshold, model_path):
     find this out before passing
     threshold: float, for flagging as anomalous
     model_path: string, path to saved model
+    anomaly_type: string, what to name the anomaly column (eg manual_anomaly, model_anomaly, or realtime_anomaly)
     """
     # make predictions
     model = keras.models.load_model(model_path + model_id)
@@ -25,14 +28,14 @@ def make_prediction(model_id, x_data, time_stamps, threshold, model_path):
     test_mae_loss = np.mean(np.abs(x_test_pred - x_data), axis=1)
     test_score_df = pd.DataFrame(time_stamps, columns=["Timestamp"])
     test_score_df["loss"] = test_mae_loss
-    test_score_df["threshold"] = threshold
-    test_score_df["anomaly"] = test_score_df["loss"] > test_score_df["threshold"]
+    test_score_df["DateTime"] = threshold
+    test_score_df[anomaly_type] = test_score_df["loss"] > test_score_df["threshold"]
 
     return_data = {
-        "Timestamp": test_score_df["Timestamp"].values.tolist(),
+        "DateTime": test_score_df["DateTime"].values.tolist(),
         "loss": test_score_df["loss"].values.tolist(),
         "threshold": test_score_df["threshold"].values.tolist(),
-        "anomaly": test_score_df["anomaly"].values.tolist(),
+        anomaly_type: test_score_df["anomaly"].values.tolist(),
     }
 
     return {
