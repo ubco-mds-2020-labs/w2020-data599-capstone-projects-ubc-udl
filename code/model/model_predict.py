0441586f80def3b3b7e92230a2064bf15a4541d5
Mitch Harris <mitchharris@live.ca>
2021-06-16 21:19:28 -0700

    clean up notebook, add results' theshold/window/sequencing function

--- code/model/model_predict.py
+++ code/model/model_predict.py
@@ -9,7 +9,14 @@ import numpy as np
 
 
 def make_prediction(
-    model_id, x_data, time_stamps, threshold, model_path, anomaly_type="anomaly"
+    model_id,
+    x_data,
+    time_stamps,
+    threshold,
+    values,
+    model_path,
+    anomaly_type="anomaly",
+    manual_anomaly=None,
 ):
     """
     model_id: string, name of the model to load
@@ -17,8 +24,10 @@ def make_prediction(
     time_stamps: time_stamps: list, timestamps of prediction points,
     find this out before passing
     threshold: float, for flagging as anomalous
+    values: unscaled values to be added back to the resulting dataframe
     model_path: string, path to saved model
     anomaly_type: string, what to name the anomaly column (eg manual_anomaly, model_anomaly, or realtime_anomaly)
+    extra: pandas series, eg if passing in manual anomalies
     """
     # make predictions
     model = keras.models.load_model(model_path + model_id)
@@ -26,20 +35,14 @@ def make_prediction(
 
     # format predictions
     test_mae_loss = np.mean(np.abs(x_test_pred - x_data), axis=1)
-    test_score_df = pd.DataFrame(time_stamps, columns=["Timestamp"])
+    test_score_df = pd.DataFrame(time_stamps, columns=["DateTime"])
     test_score_df["loss"] = test_mae_loss
-    test_score_df["DateTime"] = threshold
+    test_score_df["threshold"] = threshold
     test_score_df[anomaly_type] = test_score_df["loss"] > test_score_df["threshold"]
+    test_score_df["uniqueID"] = model_id
+    test_score_df["val_num"] = values
+    if manual_anomaly is not None:
+        test_score_df["manual_anomaly"] = manual_anomaly
+    test_score_df.set_index("DateTime", drop=True, inplace=True)
 
-    return_data = {
-        "DateTime": test_score_df["DateTime"].values.tolist(),
-        "loss": test_score_df["loss"].values.tolist(),
-        "threshold": test_score_df["threshold"].values.tolist(),
-        anomaly_type: test_score_df["anomaly"].values.tolist(),
-    }
-
-    return {
-        "version": "0.0.1",
-        "data": return_data,
-        "model_id": model_id,
-    }
+    return test_score_df
