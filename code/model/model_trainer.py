fix off by one error

code/model/model_trainer.py
@@ -18,11 +18,11 @@ THRESHOLD = 1.5
 TIME_STEPS = 15
 
 
-def create_sequences(X, y, time_steps=30, window=1):
+def create_sequences(X, y, time_steps=15, window=1):
     Xs, ys = [], []
-    for i in range(0, len(X) - time_steps, window):
+    for i in range(0, len(X) - time_steps + 1, window):
         Xs.append(X.iloc[i : (i + time_steps)].values)
-        ys.append(y.iloc[i + time_steps])
+        ys.append(y.iloc[i + time_steps - 1])
     return np.array(Xs), np.array(ys)
 
 

code/model/sensor_predict.py
@@ -62,8 +62,8 @@ for key, df in dfs_for_pred.items():
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
     # set up lists for passing to predict
-    timestamps = df["DateTime"].tail(len(df) - x_train.shape[1]).values
-    val_nums = df["Value"].tail(len(df) - x_train.shape[1]).values
+    timestamps = df["DateTime"].tail(len(df) - x_train.shape[1] + 1).values
+    val_nums = df["Value"].tail(len(df) - x_train.shape[1] + 1).values
 
     loss_percentile = cl.load_loss_percentile(key, file_path=PERCENTILE_SAVE_LOC)
     threshold = threshold_ratios[key] * loss_percentile